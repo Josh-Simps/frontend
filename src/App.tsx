873cfd98575068d59f32712a20ee56f4dc2bcf72
Tinchuu <65702538+Tinchuu@users.noreply.github.com>
2023-08-05 17:22:08 +1200

    added some empty book browser content

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BookPage from "./BookPage";
+import BookBrowser from "./BookBrowser";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1> Hello World</h1>} />
+        <Route path="/browser" Component={BookBrowser} />
         <Route path="/book" Component={BookPage}/>
       </Routes>
     </BrowserRouter>
