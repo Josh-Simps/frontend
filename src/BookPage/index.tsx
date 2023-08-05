3311b64d7e62c4b00cd6bdc8c29518dd70c003c3
luorixo <luorixo@gmail.com>
2023-08-05 16:05:34 +1200

    styling to book page

--- src/BookPage/index.tsx
+++ src/BookPage/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./BookPage.css";
 
 const content = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus iaculis vehicula. Mauris mollis orci ut ullamcorper vulputate. Integer sagittis sed mauris non posuere. Nunc consequat, nisi quis iaculis interdum, erat dui viverra dolor, sit amet consectetur orci lectus in est. Fusce id mi ut lorem maximus facilisis eu laoreet tortor. Donec est leo, porttitor a dapibus a, consequat ac tortor. Quisque et massa pulvinar, semper ante id, consequat dui. Morbi at feugiat lorem, sed facilisis nisl. Donec blandit quam dui, quis eleifend dolor posuere sed. Nulla rutrum id magna sit amet lacinia. Cras quis vestibulum ex. Integer ornare massa nibh, sit amet posuere purus sagittis consectetur. Mauris in maximus ligula. Curabitur risus mauris, rutrum vitae orci euismod, hendrerit elementum metus. Cras vehicula, nibh et cursus vestibulum, sapien diam faucibus tortor, tristique dictum erat lorem sit amet nisi. Sed et tincidunt justo, non sollicitudin lorem. Vivamus ut enim faucibus, faucibus quam id, feugiat sem. Sed a sagittis leo. Pellentesque eu semper odio. Maecenas scelerisque laoreet libero, elementum tincidunt elit scelerisque eget. Vestibulum iaculis, ante nec sollicitudin volutpat, augue sem vestibulum lectus, ac dictum nibh ipsum nec ligula. Etiam in urna id erat faucibus pulvinar eget id mi. Donec ac sem nisl. Donec varius massa eu pharetra pretium. Sed lorem orci, tristique id convallis at, efficitur pharetra elit. Suspendisse sagittis nunc eget ullamcorper dapibus. In tincidunt mollis varius. Mauris elementum gravida mi, et rutrum libero rhoncus eget. Duis vel velit dui. Aenean maximus tincidunt enim. Curabitur faucibus lorem vitae ligula tempus, vitae gravida ipsum maximus. Ut efficitur orci vitae purus tincidunt tincidunt. Cras non quam ut nibh rhoncus mollis."
-
+const content2 = "It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him. The hallway smelt of boiled cabbage and old rag mats. At one end of it a coloured poster, too large for indoor display, had been tacked to the wall. It depicted simply an enormous face, more than a metre wide: the face of a man of about forty-five, with a heavy black moustache and ruggedly handsome features. Winston made for the stairs. It was no use trying the lift. Even at the best of times it was seldom working, and at present the electric current was cut off during daylight hours. It was part of the economy drive in preparation for Hate Week. The flat was seven flights up, and Winston, who was thirty-nine and had a varicose ulcer above his right ankle, went slowly, resting several times on the way. On each landing, opposite the lift-shaft, the poster with the enormous face gazed from the wall. It was one of those pictures which are so contrived that the eyes follow you about when"
 export interface BookType {
   title: string
   coverImage: string
@@ -48,8 +48,8 @@ const Page = React.forwardRef<HTMLDivElement, PageType>((props, ref) => {
         <img src={props.img}></img>
         <h5>{props.pageContent}</h5>
 
-        <div className="pageCorner">
-          <h2>Page {props.pageNumber}</h2>
+        <div className="page-corner">
+          <h5>{props.pageNumber}</h5>
         </div>
 
       </div>
@@ -66,9 +66,9 @@ function BookPage() {
       </div>
 
       <div id="book">
-        <HTMLFlipBook width={500} height={500} size="stretch" drawShadow={true} >
+        <HTMLFlipBook width={500} height={480} size="stretch" drawShadow={true} >
           <PageCover title="Hello"></PageCover>
-          <Page img="https://i.imgur.com/5yQqB1T.jpg" pageContent={content} pageNumber="1"></Page>
+          <Page img="https://i.imgur.com/5yQqB1T.jpg" pageContent={content2} pageNumber="1"></Page>
           <Page img="https://i.imgur.com/5yQqB1T.jpg" pageContent={content} pageNumber="1"></Page>
           <Page img="https://i.imgur.com/5yQqB1T.jpg" pageContent={content} pageNumber="1"></Page>
           <Page img="https://i.imgur.com/5yQqB1T.jpg" pageContent={content} pageNumber="1"></Page>
