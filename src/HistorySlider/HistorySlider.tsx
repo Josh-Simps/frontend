e5c174b566b7d999f566997885a5a5377dd886f7
pumbas600 <joshjeffers600@gmail.com>
2023-08-06 14:37:47 +1200

    style :lipstick: Style the slider

--- src/HistorySlider/HistorySlider.tsx
+++ src/HistorySlider/HistorySlider.tsx
@@ -1,41 +1,62 @@
-import React, { ChangeEvent } from 'react';
-import Slider from '@mui/material/Slider';
-import "./HistorySlider.css";
-import { Typography } from '@mui/material';
+import React from 'react'
+import Slider, { sliderClasses } from '@mui/material/Slider'
+import './HistorySlider.css'
+import { styled } from '@mui/material'
 
 interface HistorySliderProps {
-    defaultValue?: number;
-    onSliderChange: (newValue: number) => void;
+  defaultValue?: number
+  onSliderChange: (newValue: number) => void
 }
 
+const StyledSlider = styled(Slider)({
+  color: '#ece3c2',
+  height: 4,
+  [`& .${sliderClasses.mark}`]: {
+    width: 5,
+    height: 5,
+    backgroundColor: 'black',
+    borderRadius: '50%',
+  },
+  [`& .${sliderClasses.track}`]: {
+    height: 2,
+  },
+  [`& .${sliderClasses.rail}`]: {
+    opacity: 1,
+  },
+  [`& .${sliderClasses.markLabel}`]: {
+    color: 'white',
+    fontWeight: 'inherit',
+  },
+})
+
 const marks = [
-    {
-      value: 0,
-      label: 'Symbols',
-    },
-    {
-      value: 50,
-      label: '15th Century English',
-    },
-    {
-      value: 100,
-      label: 'English',
-    },
-  ];
+  {
+    value: 0,
+    label: 'Symbols',
+  },
+  {
+    value: 50,
+    label: '15th Century English',
+  },
+  {
+    value: 100,
+    label: 'English',
+  },
+]
 
 const HistorySlider: React.FC<HistorySliderProps> = ({ defaultValue = 0, onSliderChange }) => {
-  const [sliderValue, setSliderValue] = React.useState<number>(defaultValue);
+  const [sliderValue, setSliderValue] = React.useState<number>(defaultValue)
 
-  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+  const handleSliderChange = (_event: Event, newValue: number | number[]) => {
     if (typeof newValue === 'number') {
-        setSliderValue(newValue);
-        onSliderChange(newValue);
+      setSliderValue(newValue)
+      onSliderChange(newValue)
     }
-  };
+  }
 
   return (
     <div className="sliderBody">
-      <Slider
+      <StyledSlider
         value={sliderValue}
         onChange={handleSliderChange}
         aria-label="Slider"
@@ -47,7 +68,7 @@ const HistorySlider: React.FC<HistorySliderProps> = ({ defaultValue = 0, onSlide
         color="primary"
       />
     </div>
-  );
-};
+  )
+}
 
-export default HistorySlider;
+export default HistorySlider
