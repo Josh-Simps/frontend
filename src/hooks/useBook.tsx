ba9f76626b40398ca036d286650d88069eff76f3
pumbas600 <joshjeffers600@gmail.com>
2023-08-06 12:55:24 +1200

    feat: :sparkles: Cache books after fetching them

--- src/hooks/useBook.tsx
+++ src/hooks/useBook.tsx
@@ -2,20 +2,32 @@ import { useEffect, useState } from 'react'
 import { Book } from '../services/BookData'
 import { Config } from '../config'
 import { isAbortError } from '../utils/requestUtils'
+import { useBookContext } from '../contexts/BookContext'
 
 export const useBook = (bookId?: string) => {
+  const { getBook, insertBook } = useBookContext()
   const [error, setError] = useState<unknown | null>(null)
   const [book, setBook] = useState<Book | null>(null)
 
   useEffect(() => {
     if (bookId !== undefined && (book === null || book._id !== bookId)) {
+      setError(null)
+
+      const cachedBook = getBook(bookId)
+      if (cachedBook) {
+        setBook(cachedBook)
+        return
+      }
+
       const controller = new AbortController()
       const signal = controller.signal
 
-      setError(null)
       fetch(`${Config.BackendBaseUrl}/api/books/${bookId}`, { signal })
         .then((response) => response.json())
-        .then((data) => setBook(data))
+        .then((data) => {
+          setBook(data)
+          insertBook(data)
+        })
         .catch((err) => {
           if (!isAbortError(err)) {
             setError(err)
@@ -24,7 +36,7 @@ export const useBook = (bookId?: string) => {
 
       return () => controller.abort()
     }
-  }, [book, bookId])
+  }, [book, bookId, getBook, insertBook])
 
   const isLoading = error === null && book?._id !== bookId
 
